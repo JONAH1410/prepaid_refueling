1daa3c86be3516cff796aaf9716801787d204e29
JONAHJACOB <Jonah.jacob85@gmail.com>
2024-11-08 14:07:45 +0300

    Add files via upload

---
+++ db.php
@@ -0,0 +1,12 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "Ja@deni14109021";
+$dbname = "refuel_system";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
